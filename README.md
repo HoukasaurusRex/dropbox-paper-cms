4667b4297a639116f2ea1e4fd956a27be7002382
Pterobyte <jt1992@gmail.com>
2019-06-02 19:07:59 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,90 @@
-# dropbox-paper-cms
+# Dropbox Paper CMS
+
 Using Dropbox Paper APIs to generate static content in Vuepress
+
+## Screenshots
+
+Gifs or screenshots of the project.
+
+[![prs welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg)](https://github.com/pterobyte/dropbox-paper-cms/master)
+[![styled with prettier](https://img.shields.io/badge/styled_with-prettier-ff69b4.svg)](https://github.com/prettier/prettier)
+
+One Paragraph of project description goes here
+
+## Contents  
+
+- [Examples](#examples)
+- [Contributing](#contributing)  
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+### Prerequisites
+
+What things you need to install the software and how to install them
+
+```
+Give examples
+```
+
+### Installing
+
+A step by step series of examples that tell you how to get a development env running
+
+Say what the step will be
+
+```
+Give the example
+```
+
+And repeat
+
+```
+until finished
+```
+
+End with an example of getting some data out of the system or using it for a little demo
+
+## Running the tests
+
+Explain how to run the automated tests for this system
+
+### Break down into end to end tests
+
+Explain what these tests test and why
+
+```
+Give an example
+```
+
+### And coding style tests
+
+Coding styles (e.g. airbnb, google, standard)
+Explain what these tests test and why
+
+```
+Give an example
+```
+
+## Contributing
+
+Please read [CONTRIBUTING.md](#) for details on our code of conduct, and the process for submitting pull requests to us.
+
+## Versioning
+
+We use [SemVer](http://semver.org/) for versioning. For the versions available, see the [tags on this repository](https://github.com/your/project/tags). 
+
+## Authors
+
+* **JT Houk** - [Pterobyte](https://github.com/pterobyte)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details
+
+## Acknowledgments
+
+* Hat tip to anyone whose code was used
+* Inspiration
+* etc
---
+++ index.js
@@ -0,0 +1,265 @@
+/**
+ * @typedef {{id:string,folderInfo:{folders:[]}}} DocWithFolderInfo
+ * @typedef {{id:string,folders:[{}],metaData:{},content:string}} DocWithContent
+ * @typedef {{id:string,folders:[{}],metaData:{},content:string,location:string}} Doc
+ */
+const path = require('path')
+const { mkdir, writeFile } = require('fs').promises
+const fetch = require('node-fetch')
+const promiseAllProps = require('promise-all-props')
+const { kebabCaseIt } = require('case-it')
+
+const dropboxApiBaseUrl = 'https://api.dropboxapi.com/2/paper'
+const listDocsUrl = `${dropboxApiBaseUrl}/docs/list`
+const getFolderInfoUrl = `${dropboxApiBaseUrl}/docs/get_folder_info`
+const getMetaDataUrl = `${dropboxApiBaseUrl}/docs/get_metadata`
+const downloadContentUrl = `${dropboxApiBaseUrl}/docs/download`
+
+let token
+let vueConfig
+let dir
+
+/**
+ * JSON to Frontmatter
+ * 
+ * @description converts json to frontmatter used in markdown headers
+ * @param {{}} json 
+ * @returns {String} Frontmatter String
+ */
+const jsonToFrontmatter = json => `---\n${ JSON.stringify(json, null, 2) }\n---\n`;
+
+/**
+ * Get Folder Name
+ * 
+ * @description gets first-nested folder name from array of folders
+ * @param {[]} folders 
+ * @returns {String} Folder Name
+ */
+const getFolderName = (folders) => folders.slice(1)[0].name // slice out parent directory
+
+/**
+ *  Get Folder Path
+ * 
+ * @description returns a path string from a paper api folderInfo object
+ * @param {{ folders:[] }} folderInfo 
+ * @returns {String} Path
+ */
+const getFolderPath = (dir, folderInfo) => path.join(dir, getFolderName(folderInfo.folders))
+
+
+/**
+ * POST Get Paper Content
+ * 
+ * @description downloads content of document in markdown
+ * @param {String} url 
+ * @param {String} docId 
+ * @returns {Promise<String>} Content
+ */
+const postGetPaperContent = async(url, docId) => {
+  const options = {
+    method: 'POST',
+    headers: { 
+      'Dropbox-API-Arg': JSON.stringify({
+        doc_id: docId,
+        export_format: 'markdown',
+      }),
+      'Authorization': `Bearer ${token}`
+    },
+  }
+  const res = await fetch(url, options)
+  if (res.ok) {
+    return res.text()
+  }
+  const errorMsg = await res.text()
+  throw new Error(errorMsg)
+}
+
+/**
+ * POST Get Paper Data
+ * 
+ * @description gets document data from paper api
+ * @param {String} url 
+ * @param {{}} body
+ * @returns {{}} Paper Data
+ */
+const postGetPaperData = async(url, body = {}) => {
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    },
+    body: JSON.stringify(body)
+  }
+  const res = await fetch(url, options)
+  if (res.ok) {
+    return res.json()
+  }
+  const errorMsg = await res.text()
+  throw new Error(errorMsg)
+}
+
+/**
+ * Update Sidebar
+ *
+ * @description updates vueConfig with pages tabs and arrays
+ * @param {[String]} pageLists
+ */
+const updateSidebar = (pageLists) => {
+  pageLists.forEach((list) => {
+    vueConfig.themeConfig.sidebar[list.tab] = list.pages
+  })
+}
+
+/**
+ * Only Unique
+ * @param {*} value 
+ * @param {number} index 
+ * @param {[]} self 
+ * @returns {boolean} Is Unique
+ */
+function onlyUnique(value, index, self) { 
+  return self.indexOf(value) === index;
+}
+
+/**
+ * Return Pages List
+ * 
+ * @description returns a list of page titles
+ * @param {[Doc]} docs 
+ * @returns {[String]} Pages List
+ */
+const returnPagesList = (docs, tab) => {
+  const pages = ['']
+  docs
+    .filter((doc) => doc.folders[1].name === tab)
+    .forEach((doc) => {
+      const title = kebabCaseIt(doc.metaData.title)
+      pages.push(title)
+    })
+  pages.sort()
+  return pages
+}
+
+/**
+ * Append Folder Info
+ * 
+ * @description appends folder info to docs
+ * @param {[{id:string}]} docs 
+ * @returns {Promise<DocWithFolderInfo>} Docs
+ */
+const appendFolderInfo = (docs) => Promise.all(
+  docs.doc_ids.map(id => promiseAllProps({
+    id,
+    folderInfo: postGetPaperData(getFolderInfoUrl, { doc_id: id })
+  }))
+)
+
+/**
+ * Filter Empty
+ * 
+ * @description filters out empty docs
+ * @param {[DocWithFolderInfo]} docs 
+ * @returns {[DocWithFolderInfo]} Docs
+ */
+const filterEmpty = (docs) => docs.filter(doc => doc.folderInfo.folders && doc.folderInfo.folders.length > 1)
+
+
+/**
+ * Append Doc Content
+ * 
+ * @description appends content to docs
+ * @param {[DocWithFolderInfo]} docs
+ * @returns {Promise<[DocWithContent]>}  Docs
+ */
+const appendDocContent = (docs) => Promise.all(
+  docs.map(({ id, folderInfo }) => promiseAllProps({
+    id,
+    folders: folderInfo.folders,
+    directory: getFolderPath(dir, folderInfo),
+    metaData: postGetPaperData(getMetaDataUrl, { doc_id: id }),
+    content: postGetPaperContent(downloadContentUrl, id),
+  }))
+)
+
+/**
+ * Append Doc Locations
+ * 
+ * @description appends file locations to docs
+ * @param {[DocWithFolderInfo]} docs
+ * @returns {[DocWithContent]}
+ */
+const appendDocLocations = (docs) => docs.map((doc) => ({
+  ...doc,
+  location: path.join(doc.directory, `${kebabCaseIt(doc.metaData.title)}.md`)
+}))
+
+/**
+ * Save Docs Locally
+ * 
+ * @description writes docs to files locally
+ * @param {[Doc]} docs 
+ * @returns {[Doc]} Docs
+ */
+const saveDocsLocally = (docs) => {
+  docs.forEach(doc => {
+    mkdir(doc.directory, { recursive: true })
+      .then(() => 
+        writeFile(doc.location, `${jsonToFrontmatter(doc.metaData)}${doc.content}`))
+
+    writeFile(`${dir}/meta-tree.json`, JSON.stringify(
+      docs.map(({ id, folders, metaData, location }) => ({
+        id,
+        folders,
+        content: { metaData },
+        location,
+      }))
+    ))
+  })
+  return docs
+}
+
+/**
+ * Generate Sidebar Content
+ *
+ * @description filters folders out from docs and adds page names and tabs to vueconfig
+ * @param {[Doc]} docs
+ * @returns {[Doc]} Docs
+ */
+const generateSidebarContent = async(docs) => {
+  const tabs = docs
+    .map(doc => doc.folders[1].name)
+    .filter(onlyUnique)
+  const pageLists = tabs.map((tab) => ({
+    tab: `/${tab}/`,
+    pages: returnPagesList(docs, tab)
+  }))
+  updateSidebar(pageLists)
+  console.log(JSON.stringify(pageLists, undefined, 2))
+  await writeFile(`${dir}/.vuepress/config.js`, `module.exports = ${JSON.stringify(vueConfig, undefined, 2)}`)
+  return docs
+}
+
+/**
+ * Paper CMS
+ * 
+ * @description fetches dropbox paper files in markdown, writes to files in contentDir, then updates CMS config sidebar with new file paths
+ * @param {String} dropboxApiToken API token for Dropbox API
+ * @param {JSON} config CMS JSON or JS config file
+ * @param {String} contentDir relative path to content directory
+ * @returns {[Doc]} Docs
+ */
+const paperCMS = (dropboxApiToken, config, contentDir) => {
+  token = dropboxApiToken
+  vueConfig = config
+  dir = contentDir
+  return postGetPaperData(listDocsUrl)
+    .then(appendFolderInfo)
+    .then(filterEmpty)
+    .then(appendDocContent)
+    .then(appendDocLocations)
+    .then(saveDocsLocally)
+    .then(generateSidebarContent)
+}
+
+module.exports = paperCMS
